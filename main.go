64135de096571a16d4f507ac89b2e447aabe7c4b
Haobo Wang <379723120@qq.com>
2022-12-26 21:52:57 +0800

    init

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/bubbly-hb/blogSystem-gin-vue/controller"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	r := gin.Default()
+	r.GET("/ping", controller.Ping)
+	r.Run(":8080")
 	fmt.Println("blogSystem-gin-vue")
 }
